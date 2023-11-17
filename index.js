17d5d9767d3f245a50e8b35417b26c82e9730907
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-11-17 14:41:57 +0100

    milestone 2,modell,event class

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const express = require("express");
 // importo dotenv
 const dotenv = require("dotenv");
 dotenv.config();
-
-
+// importo il router
 const eventsRouter = require ('./router/events');
+// importo il model
+const singelEvent = require('./models/event');
+
+//********************** Creazione di un'istanza del models **********************
+const event = new singelEvent(1, 'zoom meeting', 'corso React Boolean', '2023-12-01', 26);
+console.log(event instanceof singelEvent); 
+
+singelEvent.createEvent(1, 'zoom meeting', 'corso React Boolean', '2023-12-01', 26);
+console.log(event);
+//********************** Fine Creazione di un'istanza del models **********************
+
 // istanza di express
 const app = express();
 
-// Rotte relative all'entità events
-app.use("/events", eventsRouter)
+// ********************** Rotte relative all'entità events **********************
+app.use("/events", eventsRouter);
+
+
 
 
 // Avviamo il server
---
+++ models/event.js
@@ -0,0 +1,102 @@
+
+// Milestone 2
+
+//     Creiamo il model (classe) models/event.js e prevediamo le seguenti proprietà:
+
+//     id
+//     title
+//     description
+//     date
+//     maxSeats (numero massimo di posti)
+
+
+
+const fs = require('fs');
+const path = require('path');
+
+
+class singelEvent 
+{
+    id;
+    title;
+    description;
+    date;
+    maxSeats;
+
+ /**
+    *  @param {number} id
+     * @param {string} title
+     * @param {string} description
+     * @param {number} date
+     * @param {number} maxSeats
+     * 
+    */
+
+ constructor(id,title,description,date,maxSeats,) 
+    {
+        this.id=id;
+        this.title=title;
+        this.description=description;
+        this.date=date;
+        this.maxSeats=maxSeats;
+        
+    }   
+
+
+// leggo il contenuto e restituisco eventuali errori
+    static readEvents() {
+        try {
+            
+            // Leggo il contenuto del db come stringa
+            const eventData = fs.readFileSync(path.resolve(__dirname, '../db/events.json'), 'utf8');
+            return eventData ? JSON.parse(eventData) : ('array vuoto');
+        } catch (error) {
+            // Gestisco gli errori durante la lettura del file JSON
+            console.error('Errore durante la lettura del file JSON');
+            return [];
+        }
+    }
+
+    /**
+     * 
+     * @param {Array} events 
+     */
+    // scrivo sul db
+    static writeEvents(events) {
+        try {
+         
+            // Scrivo l'array di eventi nel file JSON, con formattazione per la leggibilità
+            fs.writeFileSync(path.resolve(__dirname, '../db/events.json'), JSON.stringify(events, null, 2), 'utf8');
+        } catch (error) {
+            // Gestisco gli errori durante la scrittura nel file JSON
+            console.error('Errore durante la scrittura del file JSON:');
+        }
+    }
+
+    static createEvent(id, title, description, date, maxSeats) {
+        // Leggo tutti gli eventi esistenti
+        const existingEvents = singelEvent.readEvents();
+
+        // Verifico se esiste già un evento con lo stesso ID
+        const existingEvent = existingEvents.find(event => event.id === id);
+
+        // Se non esiste, aggiungo il nuovo evento e lo salvo nel file JSON
+        if (!existingEvent) {
+            // Creo un nuovo evento
+            const newEvent = new singelEvent(id, title, description, date, maxSeats);
+            // Aggiungo il nuovo evento agli eventi esistenti
+            existingEvents.push(newEvent);
+
+            // Scrivo i dati nel file JSON
+            singelEvent.writeEvents(existingEvents);
+
+            console.log('Evento creato è stato salvato con successo.');
+        } else {
+            console.log('L\'evento con lo stesso ID già esiste.');
+        }
+    }
+}
+ 
+module.exports = singelEvent;
+
+
